1f4acdd3b7181cd5725b60b071f3149e3ad327dc
윤 해인 <woovwave8.o0@gmail.com>
2026-09-14 17:18:42 +0900

    feat: 9.PaymentService 개발2

--- src/main/java/org/example/Payment.java
+++ src/main/java/org/example/Payment.java
@@ -1,4 +1,61 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+    // 요청 사항 6개의 필드를 추가
+    private Long orderId; // 주문번호
+    private String currency; // 외국 통화 종류
+    private BigDecimal foreignCurrencyAmount; // 기준 결제 금액(부동소수점 오차가 없는 BigDecimal을 꼭 사용할 것!!!)
+
+    private BigDecimal exRate; // 적용 환율
+    private BigDecimal convertedAmount; // 원화 환산금액
+    private LocalDateTime validUntil; // 유효시간
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    // setter보다 생성자를 이용하는 방법을 추천.
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
--- src/main/java/org/example/PaymentService.java
+++ src/main/java/org/example/PaymentService.java
@@ -1,14 +1,22 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() { // payment타입 객체를 return하는 prepare함수(매개변수x)
-        return new Payment();
+    // 주문번호, 외국통화종류, 외국 통화 기준 결제 금액 request
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) { // payment타입 객체를 return하는 prepare함수(매개변수x)
+        // TODO 1. 환율 가져오기
+        // TODO 2. 금액 계산
+        // TODO 3. 유효시간 계산
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //static 메서드니까 prepare를 바로 실행 불가. 변수에 담아야함.
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(); // 변수에 담아야 함
-        System.out.println(payment); // 페이먼트 타입이 출력됨
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7)); // 변수에 담음
+        System.out.println(payment); // payment 타입이 출력됨
     }
 
 }
